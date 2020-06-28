33845a7685bffbece78276e4b97cbb582a17f05c
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-06-28 18:36:33 +0200

    function if working

---
+++ middleCharacter.js
@@ -0,0 +1,12 @@
+function middleChar(str){
+  if (str.length % 2 === 0){
+    let middle = str.length / 2;
+    return str[middle-1]+str[middle];
+  }
+  else{
+    let middle = Math.round(str.length / 2);
+    return str[middle-1];
+  }
+}
+
+middleChar("abcdefg")
