d5d6f39b10ee20d06d678c56d1863306e1ead19d
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-07-03 15:15:36 +0200

    used with switch statement

--- whoLikesIt.js
+++ whoLikesIt.js
@@ -1,24 +1,39 @@
 function likes(names) {
-  if (names.length === 0) {
-    return `no one likes this`;
+  switch(names.length) {
+    case 0:
+      return "no one likes this";
+      break;
+    case 1:
+      return `${names[0]} likes this`;
+      break;
+    case 2:
+      return `${names[0]} and ${names[1]} like this`;
+      break;
+    case 3:
+      return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+      break;
   }
-  else if (names.length === 1) {
-    return `${names[0]} likes this`;
-  }
-  else if (names.length === 2) {
-    return `${names[0]} and ${names[1]} like this`;
-  }
-  else if (names.length ===3 ) {
-    return `${names[0]}, ${names[1]} and ${names[2]} like this`;
-  }
-  else {
+  if (names.length > 3) {
     return `${names[0]}, ${names[1]} and ${(names.length - 2)} others like  this`;
   }
 }
 
-
-
-
+//another way (it works):
+// if (names.length === 0) {
+//   return `no one likes this`;
+// }
+// else if (names.length === 1) {
+//   return `${names[0]} likes this`;
+// }
+// else if (names.length === 2) {
+//   return `${names[0]} and ${names[1]} like this`;
+// }
+// else if (names.length === 3 ) {
+//   return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+// }
+// else {
+//   return `${names[0]}, ${names[1]} and ${(names.length - 2)} others like  this`;
+// }
 
 likes ([]) // must be "no one likes this"
 likes (["Peter"]) // must be "Peter likes this"
