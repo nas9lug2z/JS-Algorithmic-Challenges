ca13e05c0cb56adc8e344c58b34968780b3e7f0b
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-07-21 20:50:27 +0200

    bulky version, but to keep it

--- casinoChips.js
+++ casinoChips.js
@@ -1,9 +1,10 @@
 function casinoChips (arr) {
+
+
     let counter = 0;
-    let stop = 0;
-    while (stop === 0)  {
+    let stop = false;
+    while (stop === false)  {
         let zeroCount = 0;
-        console.log(`initial array in a while loop is ${arr}`);
         //find first minimum
         let min = arr[0];
         let minI = 0;
@@ -32,14 +33,9 @@ function casinoChips (arr) {
 
             }
         }
-
         arr[minI]--;
         arr[maxI]--;
-        console.log(`now min arr[${minI}] equals to ${arr[minI]}`);
-        console.log(`now max arr[${maxI}] equals to ${arr[maxI]}`);
-        console.log(`arr now is ${arr}`)
         counter ++;
-        console.log(`counter added and total is ${counter}`)
 
         //add check
 
@@ -48,9 +44,8 @@ function casinoChips (arr) {
                 zeroCount++;
             }
         }
-        console.log(`zerocount is ${zeroCount}`)
         if (zeroCount >= 2) {
-            stop = 1;
+            stop = true;
         }
     }
     return counter;
