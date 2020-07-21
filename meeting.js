21c90b265b9773e8fda5432c728cc75617fa327e
nas9lug2z <nastia.zhyrnova@gmail.com>
2020-07-21 21:10:18 +0200

    initial commit

---
+++ meeting.js
@@ -0,0 +1,11 @@
+function meeting(s) {
+    console.log(s);
+    let arr = s.split(";");
+
+    //let's create objects
+    //we need a for loop and assign values to objects
+    console.log(arr);
+}
+
+
+meeting ("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill")
