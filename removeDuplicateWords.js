9b60e00a98d535b5513e0014d4966f3a9ebe0d69
Ana <64069789+nas9lug2z@users.noreply.github.com>
2020-10-15 08:01:54 +0200

    remove duplicates solved

--- removeDuplicateWords.js
+++ removeDuplicateWords.js
@@ -1,15 +1,6 @@
 function removeDuplicateWords (str) {
-    let arr = str.split(" ");
-    let intermed = [arr[0]];
-    let result = [arr[0]];
-    for (let i = 0; i < arr.length; i++) {
-        for (let j = 1; j < arr.length; j++) {
-            console.log(`check for j = ${j}`)
-                if (arr[i] !== arr[j]) {
-                    console.log(`${arr[i]} doesn't match ${arr[j]}`)
-                }
-        }
-    }
+        const set = new Set(str.split(" "));
+        return Array.from(set).join(" ");
 
   }
 
