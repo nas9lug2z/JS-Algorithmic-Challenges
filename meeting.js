118acf14359d7e1ce2eeea3c5593f68b818603a4
Ana <64069789+nas9lug2z@users.noreply.github.com>
2020-10-31 11:50:29 +0100

    meeting solved

--- meeting.js
+++ meeting.js
@@ -1,22 +1,33 @@
 function meeting(s) {
-    let unsortedArr = [];
-    for (let item of s.split(";")) {
-        unsortedArr.push(
-            {
-                "surname": item.split(":")[1],
-                "first_name": item.split(":")[0]
-            }
-        )
-
+  let unsortedArr = [];
+  for (let item of s.split(";")) {
+    unsortedArr.push({
+      surname: item.split(":")[1].toUpperCase(),
+      first_name: item.split(":")[0].toUpperCase(),
+    });
+  }
+  unsortedArr.sort((a, b) => {
+    if (a.surname === b.surname) {
+      if (a.first_name > b.first_name) {
+        return 1;
+      } else if (a.first_name < b.first_name) {
+        return -1;
+      }
+    } else if (a.surname > b.surname) {
+      return 1;
+    } else if (a.surname < b.surname) {
+      return -1;
     }
-    unsortedArr.sort(function (a, b) {
-        let surA = a.surname.toLowerCase();
-        let surB = b.surname.toLowerCase();
-        surA > surB ? return: -1 : return 1;
-    })
-
-
+  });
+  let result = [];
+  unsortedArr.forEach((elem) => {
+    result.push(`(${elem.surname}, ${elem.first_name})`);
+  });
+  return result.join("");
 }
 
-
-meeting ("Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill")
+console.log(
+  meeting(
+    "Fred:Corwill;Wilfred:Corwill;Barney:Tornbull;Betty:Tornbull;Bjon:Tornbull;Raphael:Corwill;Alfred:Corwill"
+  )
+);
